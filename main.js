095f7c878bc9015e3187dc8a2a7b341d5fe0e389
praveen08052010 <97897309+praveen08052010@users.noreply.github.com>
2022-03-28 20:02:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-name_of_the_student_array= []
+name_of_the_student_array=[]
 
 function submit()
 {
-    var name1= document.getElementById("name_of_the_student_1").value;
-    var name2= document.getElementById("name_of_the_student_2").value;
-    var name3= document.getElementById("name_of_the_student_3").value;
-    var name4= document.getElementById("name_of_the_student_4").value;
-    
-    name_of_the_student_array.push(name1)
+    var display_student_array=[];
 
-    name_of_the_student_array.push(name2)
+    for (var j=1; j <= 4; j++)
 
-    name_of_the_student_array.push(name3)
+    {
+        var name_of_the_student=document.getElementById("name_of_the_student_"+j).value
+        console.log(name_of_the_student);
+        name_of_the_student_array.push(name_of_the_student);
+    }
 
-    name_of_the_student_array.push(name4)
+    console.log(name_of_the_student_array);
 
+    var length_of_name_of_students_array=name_of_the_students_array.length;
+    console.log(length_of_name_of_students_array);
 
+    for (var k=0; k<length_of_name_of_students_array;k++)
+    {
+       display_student_array.push("<h4>NAME -"+_name_of_student_array[k]+"/<h4>") 
+       console.log(display_student_array);
+    }
 
-    console.log(name_of_the_student_array)
+    console.log(display_student_array);
+    document.getElementById("display_name_with_commas").innerHTML=display_student_array;
 
-    document.getElementById("display_name").innerHTML=name_of_the_student_array;
+    var remove_commas=display_student_array.join("");
+    console.log(remove_commas);
+    document.getElementById("display_name_with_commas").innerHTML=remove_commas;
+
+    document.getElementById("submit_button").style.display= "none";
 }
-    
